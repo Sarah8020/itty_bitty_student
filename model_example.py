848512c66ac09fa190add52ceda8eda706d5beaa
MuhammadH <Muhammadhauris@gmail.com>
2022-03-28 12:56:13 -0600

    added example

---
+++ model_example.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+from numpy import load
+
+if __name__ == '__main__':
+    # open model
+    student = tf.keras.models.load_model('student.model')
+    
+    # make 2 test inputs
+    test_set = []
+    test = []
+    for i in range (3000):
+        test.append(0.0)
+    test2 = []
+    for i in range (1500):
+        test2.append(10.0)
+        test2.append(-10.0)
+    test_set.append(test)
+    test_set.append(test2)
+    
+    # converting to numpy not neccessary i guess?
+    #np_set = np.array(test_set)
+    predictions = student.predict(test_set)
+    
+    print('predictions:')
+    
+    print(np.argmax(predictions[0]))
+    print(np.argmax(predictions[1]))
