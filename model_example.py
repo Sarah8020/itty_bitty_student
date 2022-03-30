6e3d539630ff32f90f4207ef4d3b0fcf410ac1d8
MuhammadH <Muhammadhauris@gmail.com>
2022-03-29 18:11:17 -0600

    added timing to example

--- model_example.py
+++ model_example.py
@@ -1,28 +1,25 @@
 import tensorflow as tf
 import numpy as np
 from numpy import load
+import time
 
 if __name__ == '__main__':
-    # open model
+    # ----open model
     student = tf.keras.models.load_model('student.model')
     
-    # make 2 test inputs
+    # ----make test inputs
     test_set = []
-    test = []
-    for i in range (3000):
-        test.append(0.0)
-    test2 = []
-    for i in range (1500):
-        test2.append(10.0)
-        test2.append(-10.0)
-    test_set.append(test)
-    test_set.append(test2)
+    for i in range(100):
+        test = [0] * 3000
+        test_set.append(test)
     
-    # converting to numpy not neccessary
-    #np_set = np.array(test_set)
+    # ----make predictions
+    start_time = time.time()
     predictions = student.predict(test_set)
+    # get prediction generation time
+    print('time to generate 100 predictions: ', time.time() - start_time)
     
+    # ----print some predictions
     print('predictions:')
-    
     print(np.argmax(predictions[0]))
     print(np.argmax(predictions[1]))
